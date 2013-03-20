8cf0614af12d0f7fce375cbfab4d472c9fe20c79
mappleconfusers <mapc@cupdev.net>
2013-03-20 09:31:43 +0100

    Rewrite in C

---
+++ chentropy.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <inttypes.h>
+
+int absmod(int i, int m) { return abs(i%m); }
+
+
+char* lib = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int libl;
+
+int minl = 5, maxl = 40;
+
+int main() {
+    //////////////////////////
+    // INIT
+    
+    libl = strlen(lib);
+
+    //////////////////////////
+    // BODY
+    char c; 
+    while(1) {
+        c = getchar();
+        int tow = minl + (absmod(c, maxl - minl));
+
+        while(tow-- > 0) {
+            c = getchar();
+            putc(lib[absmod(c, libl)], stdout);
+        }
+        putc('\n', stdout);
+    }
+
+    return 0;
+}
